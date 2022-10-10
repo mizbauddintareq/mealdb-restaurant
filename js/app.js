f22976880ae6c38c90ebc5698d27b92d5b4488c7
mizbauddintareqe <mizbauddintareq@gmail.com>
2022-10-10 22:33:53 +0600

    get search text

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 const getSearchText = () => {
-  console.log("hello");
+  const getSearchField = document.getElementById("search-field");
+  const searchText = getSearchField.value;
+  loadFoodData(searchText);
+  getSearchField.value = "";
 };
+
+const loadFoodData = async (searchText) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?f=${searchText}`;
+
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    // console.log(error);
+  }
+};
+
+loadFoodData("b");
